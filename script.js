3bb65da9a5dd9ee00bfb0320a6b801b5daf13c47
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-07-05 02:19:22 +0700

    add simple testing

---
+++ script.js
@@ -0,0 +1,16 @@
+let i = 1;
+let operator = '/';
+let n = 2;
+
+if(operator == '+'){
+    console.log(i + n);
+} 
+if(operator == '-'){
+    console.log(i - n);
+}
+if(operator == '*'){
+    console.log(i * n);
+}
+if(operator == '/'){
+    console.log(i / n);
+}
