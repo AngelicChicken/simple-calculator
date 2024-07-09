459376adcbdbf263644e30013cea075d804158a1
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-07-09 15:45:26 +0700

    can operate *-/, modify how equation is display

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const display = document.querySelector("#display");
 const numberButtons = document.querySelectorAll("#number");
+const operatorButtons = document.querySelectorAll("#operator")
 const btn = document.querySelectorAll("button");
 const equalBtn = document.querySelector("#equal");
 let input = "";
@@ -16,24 +17,39 @@ function operate(arr){
 
     if(operator == "+"){
         return num1+num2;
+    } if(operator == "-"){
+        return num1-num2;
+    } if(operator == "*"){
+        return num1*num2;
+    } if(operator == "/"){
+        return num1/num2;
     }
 }
 
-numberButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        display.textContent += button.textContent;
+// numberButtons.forEach((button) => {
+//     button.addEventListener("click", () => {
+        
+//     })
+// });
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click" () => {
+
     })
 });
 
 btn.forEach((button)=> {
     button.addEventListener("click", ()=>{
-        if(button.textContent != "=") input += button.textContent;
+        if(button.textContent != "="){
+            input += button.textContent;
+            display.textContent += button.textContent;
+        } 
         console.log(input);
     })
 });
 
 equalBtn.addEventListener("click", () =>{
-    console.log(operate(separate(input)));
+    display.textContent = operate(separate(input));
 });
 
 
