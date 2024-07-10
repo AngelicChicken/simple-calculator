6b64460e60dc08c27527b595ce2108c09b821623
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-07-10 12:12:57 +0700

    add function to update display and add 'm' if '+/-' button are clicked

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const numberButtons = document.querySelectorAll("#number");
 const operatorButtons = document.querySelectorAll("#operator");
 const resetButtons = document.querySelector("#reset");
 const percentButtons = document.querySelector("#percent");
+const minusButton = document.querySelector("#minus");
+const comaButton = document.querySelector("#coma");
+const inputButtons = document.querySelectorAll(".input");
 const btn = document.querySelectorAll("button");
 const equalBtn = document.querySelector("#equal");
 let input = "";
@@ -33,6 +36,32 @@ function percent(num){
     return num;
 }
 
+function minus(arr){
+    console.log(arr);
+    if(arr.length == 3){
+        if(arr[2].includes('m')){
+            arr[2] = arr[2].replace("m", "");
+            display.textContent = display.textContent.replace(`(-${arr[2]})`, arr[2]);
+        } else {
+            display.textContent = display.textContent.replace(arr[2], `(-${arr[2]})`);
+            arr[2] += 'm';
+        }
+    } else{
+        if(arr[0].includes('m')){
+            arr[0] = arr[0].replace("m", "");
+            display.textContent = display.textContent.replace(`(-${arr[0]})`, arr[0]);
+        } else {
+            if(arr.length == 2){
+                display.textContent = `(-${arr[0]})${arr[1]}`
+            } else {
+                display.textContent = `(-${arr[0]})`
+            }
+            arr[0] += 'm';
+        }
+    }
+    return arr.join('');
+}
+
 operatorButtons.forEach((button) => {
     button.addEventListener("click", () => {
         if(input.includes('+')||input.includes('-')||input.includes('*')||input.includes('/')){
@@ -42,17 +71,16 @@ operatorButtons.forEach((button) => {
     })
 });
 
-btn.forEach((button)=> {
+inputButtons.forEach((button)=> {
     button.addEventListener("click", ()=>{
-        if(button.textContent != "=" ||button.textContent != "%"){
-            input += button.textContent;
-            display.textContent += button.textContent;
-        }
+        input += button.textContent;
+        display.textContent += button.textContent;
     })
 });
 
 equalBtn.addEventListener("click", () =>{
-    display.textContent = operate(separate(input));
+    let separated = separate(input);
+    display.textConte3nt = operate(separated);
 });
 
 resetButtons.addEventListener("click", () => {
@@ -63,6 +91,11 @@ resetButtons.addEventListener("click", () => {
 percentButtons.addEventListener("click", () => {
     display.textContent = percent(input);
     input = percent(input);
+});
+
+minusButton.addEventListener("click", ()=> {
+    input = minus(separate(input));
+    console.log(input);
 })
 
 
